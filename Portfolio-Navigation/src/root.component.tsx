278639186b8380ae26a6d03030100e80f6f6ebce
Nick-Gianino <nickgianino4@gmail.com>
2024-04-23 23:19:03 -0500

    updated routes so that clicking on link routes you to a new MFE

--- Portfolio-Navigation/src/root.component.tsx
+++ Portfolio-Navigation/src/root.component.tsx
@@ -1,16 +1,24 @@
 import "./styles.css";
+import { navigateToUrl } from 'single-spa';
 
 export default function Root(props) {
-   return (
+  const handleResumeClick = () => {
+    navigateToUrl('/resume');
+  };
+
+  const handleAngularProjectsClick = () => {
+    navigateToUrl('/grid-project');
+  };
+
+  return (
     <>
-      <div className = "navigation-bar">
+      <div className="navigation-bar">
+        <text className="name-header">Nicholas Gianio</text>
+        <a href="#" className="navigation-links" onClick={handleResumeClick}>View Resume</a>
 
-        <text className = "name-header">Nicholas Gianio</text>
-        <a href="#" className = "navigation-links" >View Resume</a>
-
-        <text className = "react-header">React Projects</text>
-        <a href="#" className = "navigation-links" >Link to Angular Projects</a>
-
+        <text className="react-header">React Projects</text>
+        <a href="#" className="navigation-links" onClick={handleAngularProjectsClick}>Link to Angular Projects</a>
       </div>
-    </> 
-)} 
+    </>
+  );
+}

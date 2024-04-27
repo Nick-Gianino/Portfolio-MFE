b7557d6d7892e65ef9d505e825a531d36baa1d4b
Nick-Gianino <nickgianino4@gmail.com>
2024-04-26 22:09:56 -0500

    updated the root component to have loci with some content

--- Portfolio-GridProject/src/root.component.tsx
+++ Portfolio-GridProject/src/root.component.tsx
@@ -1,3 +1,51 @@
+import './root.css'
+import InterfaceableBox from './components/InterfaceableBox';
+import { useState } from 'react';
+
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+
+  const [numberOfBoxes, setNumberOfBoxes] = useState(0);
+
+  const handleAddButton = () => {
+    if(numberOfBoxes === 25){
+      return;
+    }
+    else{
+      setNumberOfBoxes(numberOfBoxes+1);
+    }
+  }
+
+  const handleSubtractButton = () => {
+    if(numberOfBoxes === 0){
+      return;
+    }else{
+      setNumberOfBoxes(numberOfBoxes-1);
+    }
+   
+  }
+
+
+  const renderBoxes = () => {
+    return Array(numberOfBoxes)
+      .fill(null)
+      .map((_, index) => <InterfaceableBox key={index} />);
+  }
+ 
+
+  return( 
+    <div className = "Grid-Container" >
+      <div className="Grid-Title-Outline">
+      <section className="Grid-Top-Section">
+        <text className="Grid-Section-Text">Please Select An Option: </text>
+         <button className="Grid-Add-Button" onClick = {handleAddButton} >Add Box</button>
+         <button className = "Grid-Minus-Button" onClick = {handleSubtractButton}>Subtract Box </button>
+      </section>
+      </div>
+      <section className = "Grid-Box-Area">
+        {renderBoxes()}
+      </section>
+      
+
+    </div>);
 }

c949569a7218bdd4ebd550ff24b77abfb44ce466
Nick-Gianino <nickgianino4@gmail.com>
2024-04-27 23:45:07 -0500

    added the draggability to the interfaceable box

--- Portfolio-GridProject/src/components/InterfaceableBox.tsx
+++ Portfolio-GridProject/src/components/InterfaceableBox.tsx
@@ -2,18 +2,32 @@ import React from 'react';
 import '../root.css';
 import {ResizableBox} from 'react-resizable';
 import 'react-resizable/css/styles.css';
+import {useDrag} from "react-dnd";
 
+
+const ItemTypes ={
+  BOX: 'box',
+}
 
 function InterfaceableBox() {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: ItemTypes.BOX,
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+
   return (
-    <ResizableBox className='InterfaceableBox'
-      resizeHandles={['sw', 'se', 'nw', 'ne', 'w', 'e', 'n', 's']}
-      height={200}
-      width={200}
-      minConstraints={[100, 100]}
-      maxConstraints={[Infinity, Infinity]}>
-        
-    </ResizableBox>
+    <div ref={drag} style ={{opacity: isDragging ? 0.5 : 1}}>
+      <ResizableBox className='InterfaceableBox'
+        resizeHandles={['sw', 'se', 'nw', 'ne', 'w', 'e', 'n', 's']}
+        height={200}
+        width={200}
+        minConstraints={[100, 100]}
+        maxConstraints={[Infinity, Infinity]}>
+          
+      </ResizableBox>
+    </div>
   )
 }
 

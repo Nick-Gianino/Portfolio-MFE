a909d1c4ba32820ce5a6355bbd48fce70157175f
Nick-Gianino <nickgianino4@gmail.com>
2024-04-28 22:28:21 -0500

    removed drag

--- Portfolio-GridProject/src/components/InterfaceableBox.tsx
+++ Portfolio-GridProject/src/components/InterfaceableBox.tsx
@@ -2,29 +2,17 @@ import React from 'react';
 import '../root.css';
 import {ResizableBox} from 'react-resizable';
 import 'react-resizable/css/styles.css';
-import {useDrag} from "react-dnd";
-
-
-const ItemTypes ={
-  BOX: 'box',
-}
 
 function InterfaceableBox() {
-  const [{ isDragging }, drag] = useDrag(() => ({
-    type: ItemTypes.BOX,
-    collect: (monitor) => ({
-      isDragging: !!monitor.isDragging(),
-    }),
-  }));
 
   return (
-    <div ref={drag} style ={{opacity: isDragging ? 0.5 : 1}}>
+    <div >
       <ResizableBox className='InterfaceableBox'
         resizeHandles={['sw', 'se', 'nw', 'ne', 'w', 'e', 'n', 's']}
         height={200}
         width={200}
         minConstraints={[100, 100]}
-        maxConstraints={[Infinity, Infinity]}>
+        maxConstraints={[500, 500]}>
           
       </ResizableBox>
     </div>

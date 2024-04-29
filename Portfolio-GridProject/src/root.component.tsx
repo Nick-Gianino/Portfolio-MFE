69932d239f97f483e4abb0dbd471f25cd6ccf769
Nick-Gianino <nickgianino4@gmail.com>
2024-04-28 22:28:12 -0500

    removed drag

--- Portfolio-GridProject/src/root.component.tsx
+++ Portfolio-GridProject/src/root.component.tsx
@@ -1,38 +1,38 @@
 import './root.css'
 import InterfaceableBox from './components/InterfaceableBox';
 import { useState } from 'react';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-
 
 export default function Root(props) {
 
   const [numberOfBoxes, setNumberOfBoxes] = useState(0);
+  const [boxesCssClass, setBoxesCssClass] = useState("");
 
 
   const renderBoxes = () => {
     return Array(numberOfBoxes)
       .fill(null)
-      .map((_, index) => <InterfaceableBox key={index} />);
+      .map((_, index) =><div key={index} className={"${boxesCssClass}"}> <InterfaceableBox key={index} /> </div>);
+  }
+  
+  const handleVisopsBoxes = () => {
+    setNumberOfBoxes(1);
+    setBoxesCssClass("Visops-Button");
+    return;
   }
  
 
   return( 
-    <DndProvider backend={HTML5Backend}>
-    <div className = "Grid-Container" >
+    <div className = "Grid-Container">
       <div className="Grid-Title-Outline">
       <section className="Grid-Top-Section">
         <text className="Grid-Section-Text">Please Select An Option: </text>
-         <button className="Grid-Add-Button" >Add Box</button>
+         <button className="Grid-Visops-Button" onClick={() => handleVisopsBoxes()}>Work Example</button>
          <button className = "Grid-Minus-Button">Subtract Box </button>
       </section>
       </div>
       <section className = "Grid-Box-Area">
         {renderBoxes()}
       </section>
-      
-
     </div>
-    </DndProvider>
   );
 }

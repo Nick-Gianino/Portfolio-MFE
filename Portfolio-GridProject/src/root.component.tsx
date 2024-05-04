66e1c761add1b1c6475081f77d923f85f249148b
Nick-Gianino <nickgianino4@gmail.com>
2024-05-03 23:43:17 -0500

    changed second button className

--- Portfolio-GridProject/src/root.component.tsx
+++ Portfolio-GridProject/src/root.component.tsx
@@ -30,7 +30,9 @@ export default function Root(props) {
       <section className="Grid-Top-Section">
         <text className="Grid-Section-Text">Please Select An Option: </text>
          <button className="Grid-Visops-Button" onClick={() => handleVisopsBoxes()}>Work Example</button>
+         <button className = "Grid-two-box-button" onClick={() => handleOtherBox()}>Test</button>
          <button className = "Grid-Minus-Button" onClick={() => handleOtherBox()}>Test</button>
+
       </section>
       </div>
       <section className = "Grid-Box-Area">

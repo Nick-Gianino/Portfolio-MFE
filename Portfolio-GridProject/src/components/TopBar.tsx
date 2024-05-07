fb4cd8956e39569cdb7f06911028028de2f8caac
Nick-Gianino <nickgianino4@gmail.com>
2024-05-06 22:27:55 -0500

    changed onOtherboxClick top new onCenterBoxClick

--- Portfolio-GridProject/src/components/TopBar.tsx
+++ Portfolio-GridProject/src/components/TopBar.tsx
@@ -4,16 +4,17 @@ import '../root.css'
 interface TopBarProps {
   onVisopsBoxesClick: () => void;
   onOtherBoxClick: () => void;
+  onCenterBoxClick: () => void;
 }
 
-const TopBar: React.FC<TopBarProps> = ({ onVisopsBoxesClick, onOtherBoxClick }) => {
+const TopBar: React.FC<TopBarProps> = ({ onVisopsBoxesClick, onOtherBoxClick, onCenterBoxClick }) => {
   return (
     <div className="Grid-Title-Outline">
       <section className="Grid-Top-Section">
         <text className="Grid-Section-Text">Please Select An Option: </text>
         <button className="Grid-Visops-Button" onClick={onVisopsBoxesClick}>Work Example</button>
         <button className="Grid-two-box-button" onClick={onOtherBoxClick}>Test</button>
-        <button className="Grid-Minus-Button" onClick={onOtherBoxClick}>Test</button>
+        <button className="Grid-Minus-Button" onClick={onCenterBoxClick}>Test</button>
       </section>
     </div>
   );

f173335207858c207241a8b9a7702665e601b785
Nick-Gianino <nickgianino4@gmail.com>
2024-05-06 22:34:21 -0500

    updated the interface to potentially use CSS Styles

--- Portfolio-GridProject/src/components/PersonalResizeable.tsx
+++ Portfolio-GridProject/src/components/PersonalResizeable.tsx
@@ -2,8 +2,9 @@ import React, { useState, useRef, useEffect } from 'react';
 
 interface ResizeableComponentProps {
     children: React.ReactNode;
-    initialWidth?: number;
-    initialheight?: number;
+    initialWidth?: string;
+    initialheight?: string;
+    style?: React.CSSProperties;
 }
 
 const PersonalResizeable = () => {

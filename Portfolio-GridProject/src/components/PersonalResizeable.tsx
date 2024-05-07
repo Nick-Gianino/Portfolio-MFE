aab9ebbe4daf24828b91182f38afdf2df3ebf2dd
Nick-Gianino <nickgianino4@gmail.com>
2024-05-06 22:44:36 -0500

    added useEffect that will be looking for the resizing listener to move the component

--- Portfolio-GridProject/src/components/PersonalResizeable.tsx
+++ Portfolio-GridProject/src/components/PersonalResizeable.tsx
@@ -19,6 +19,33 @@ const PersonalResizeable:React.FC<ResizeableComponentProps> = ({
     const [resizing, setResizing] = useState(false);
     const componentRef = useRef<HTMLDivElement>(null);
 
+    useEffect(()=> {
+        const handleMouseMove = (event: MouseEvent) => {
+            if(resizing && componentRef.current) {
+                const deltaX = event.clientX - componentRef.current.getBoundingClientRect().left;
+                const deltaY = event.clientY - componentRef.current.getBoundingClientRect().top;
+                setWidth(`${deltaX}px`);
+                setHeight(`${deltaY}px`);
+            }
+        };
+
+        const handleMouseUp = () => {
+            setResizing(false);
+        };
+
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);
+
+        return () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('mouseup', handleMouseUp);
+        };
+        }, [resizing]);
+
+        const handleMouseDown = () => {
+            setResizing(true);
+        }
+
     return (
         <div>
             

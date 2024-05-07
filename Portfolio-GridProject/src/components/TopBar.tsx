eb545240607c5cff3f3a8ae9a99b5123c91fcb2a
Nick-Gianino <nickgianino4@gmail.com>
2024-05-06 22:48:36 -0500

    changed centerbox for personalbox

--- Portfolio-GridProject/src/components/TopBar.tsx
+++ Portfolio-GridProject/src/components/TopBar.tsx
@@ -4,17 +4,17 @@ import '../root.css'
 interface TopBarProps {
   onVisopsBoxesClick: () => void;
   onOtherBoxClick: () => void;
-  onCenterBoxClick: () => void;
+  onhandlePersonalBoxClick: () => void;
 }
 
-const TopBar: React.FC<TopBarProps> = ({ onVisopsBoxesClick, onOtherBoxClick, onCenterBoxClick }) => {
+const TopBar: React.FC<TopBarProps> = ({ onVisopsBoxesClick, onOtherBoxClick, onhandlePersonalBoxClick }) => {
   return (
     <div className="Grid-Title-Outline">
       <section className="Grid-Top-Section">
         <text className="Grid-Section-Text">Please Select An Option: </text>
         <button className="Grid-Visops-Button" onClick={onVisopsBoxesClick}>Work Example</button>
         <button className="Grid-two-box-button" onClick={onOtherBoxClick}>Two Box Example</button>
-        <button className="Grid-Minus-Button" onClick={onCenterBoxClick}>Center Box Example</button>
+        <button className="Grid-Minus-Button" onClick={onhandlePersonalBoxClick}>Center Box Example</button>
       </section>
     </div>
   );

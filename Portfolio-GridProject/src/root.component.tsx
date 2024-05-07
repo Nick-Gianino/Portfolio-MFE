8f5425d390922cd63bc0aee4a31acddc23446808
Nick-Gianino <nickgianino4@gmail.com>
2024-05-06 22:55:54 -0500

    integrated the personalresizeable box on press of the third button

--- Portfolio-GridProject/src/root.component.tsx
+++ Portfolio-GridProject/src/root.component.tsx
@@ -1,7 +1,8 @@
 import './root.css'
 import InterfaceableBox from './components/InterfaceableBox';
-import { useState } from 'react';
+import { Children, useState } from 'react';
 import TopBar from './components/TopBar';
+import PersonalResizeable from './components/PersonalResizeable';
 
 export default function Root(props) {
   const [numberOfBoxes, setNumberOfBoxes] = useState(0);
@@ -10,27 +11,37 @@ export default function Root(props) {
   const renderBoxes = () => {
     return Array(numberOfBoxes)
       .fill(null)
-      .map((_, index) => <div key={index} className={`${boxesCssClass}`}> <InterfaceableBox key={index}/> </div>);
-  }
+      .map((_, index) => <div key={index} className={`${boxesCssClass}`}> {boxesCssClass === 'Personal-Box' ? (
+        <PersonalResizeable key={index}>
+          <div><h1>Personal</h1></div>
+        </PersonalResizeable>
+      ) : (
+        <InterfaceableBox key={index} />
+      )} </div>);
+  };
   
   const handleVisopsBoxes = () => {
     setNumberOfBoxes(1);
     setBoxesCssClass("Visops-Box");
-  }
+  };
 
   const handleOtherBox = () => {
     setNumberOfBoxes(2);
     setBoxesCssClass("Other-Box");
-  }
+  };
 
   const handlePersonalBox = () =>{
     setNumberOfBoxes(1);
     setBoxesCssClass("Personal-Box");
-  }
+  };
  
   return( 
     <div className="Grid-Container">
-      <TopBar onVisopsBoxesClick={handleVisopsBoxes} onOtherBoxClick={handleOtherBox} onhandlePersonalBoxClick={handlePersonalBox} />
+      <TopBar 
+        onVisopsBoxesClick={handleVisopsBoxes} 
+        onOtherBoxClick={handleOtherBox} 
+        onhandlePersonalBoxClick={handlePersonalBox} 
+        />
       <section className="Grid-Box-Area">
         {renderBoxes()}
       </section>

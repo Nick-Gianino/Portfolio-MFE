1c4fe1202a5b32b832352ac892db7ec504a5d148
Nick-Gianino <nickgianino4@gmail.com>
2024-05-06 22:38:35 -0500

    defined states for ther personal component

--- Portfolio-GridProject/src/components/PersonalResizeable.tsx
+++ Portfolio-GridProject/src/components/PersonalResizeable.tsx
@@ -3,11 +3,22 @@ import React, { useState, useRef, useEffect } from 'react';
 interface ResizeableComponentProps {
     children: React.ReactNode;
     initialWidth?: string;
-    initialheight?: string;
+    initialHeight?: string;
     style?: React.CSSProperties;
 }
 
-const PersonalResizeable = () => {
+const PersonalResizeable:React.FC<ResizeableComponentProps> = ({
+        children,
+        initialWidth = '200px', 
+        initialHeight = '200px', 
+        style={}
+    }) => {
+
+    const [width, setWidth] = useState(initialWidth);
+    const [height, setHeight] = useState(initialHeight);
+    const [resizing, setResizing] = useState(false);
+    const componentRef = useRef<HTMLDivElement>(null);
+
     return (
         <div>
             

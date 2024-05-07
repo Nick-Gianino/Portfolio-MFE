786ad5857c24380a2c6648f83d845853721a728c
Nick-Gianino <nickgianino4@gmail.com>
2024-05-06 22:33:03 -0500

    created interface for personalresizeable

--- Portfolio-GridProject/src/components/PersonalResizeable.tsx
+++ Portfolio-GridProject/src/components/PersonalResizeable.tsx
@@ -1,4 +1,10 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+
+interface ResizeableComponentProps {
+    children: React.ReactNode;
+    initialWidth?: number;
+    initialheight?: number;
+}
 
 const PersonalResizeable = () => {
     return (

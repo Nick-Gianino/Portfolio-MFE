f9a209370d562267fa5c65893ff5b6e8a180ce2e
Nick-Gianino <nickgianino4@gmail.com>
2024-05-08 23:55:30 -0500

    created portfolioroot.tsx (new location)

---
+++ Portfolio-Resume/src/components/PortfolioRoot/PortfolioRoot.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PortfolioRoot.module.css';
+
+const PortfolioRoot = () => {
+    return (
+        <div className = "RootContainer">
+            Yo
+        </div>
+    );
+};
+
+export default PortfolioRoot;

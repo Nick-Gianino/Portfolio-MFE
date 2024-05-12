7d37738766b2559ae6f0a56474b5be54d0a90b33
Nick-Gianino <nickgianino4@gmail.com>
2024-05-11 22:45:58 -0500

    added the topabr to the portfolio root

--- Portfolio-Resume/src/components/PortfolioRoot/PortfolioRoot.tsx
+++ Portfolio-Resume/src/components/PortfolioRoot/PortfolioRoot.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './PortfolioRoot.module.css';
+import TopBar from '../TopBar/TopBar';
 
 const PortfolioRoot = () => {
     return (
         <div className = "RootContainer">
-            Yo im here too also
+           <TopBar/>
         </div>
     );
 };

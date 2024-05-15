b9b841d0a350d8eeb39e09b56c77d2ef81edbb7f
Nick-Gianino <nickgianino4@gmail.com>
2024-05-14 23:23:49 -0500

    added header for work title

--- Portfolio-Resume/src/components/Portfolio-Resume/PortfolioResume.tsx
+++ Portfolio-Resume/src/components/Portfolio-Resume/PortfolioResume.tsx
@@ -4,6 +4,7 @@ const PortfolioResume = () => {
     return (
         <div className = "PortfolioResume">
             <h1 className = "NameHeader">Nicholas Gianino</h1>
+            <h2 className = "workHeader">Software Engineer</h2>
         </div>
     );
 };

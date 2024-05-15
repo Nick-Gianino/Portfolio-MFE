6c3f67406edc69d77b57c9e97369218016fa6383
Nick-Gianino <nickgianino4@gmail.com>
2024-05-14 23:23:05 -0500

    added header for name

--- Portfolio-Resume/src/components/Portfolio-Resume/PortfolioResume.tsx
+++ Portfolio-Resume/src/components/Portfolio-Resume/PortfolioResume.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PortfolioResume = () => {
     return (
         <div className = "PortfolioResume">
-            
+            <h1 className = "NameHeader">Nicholas Gianino</h1>
         </div>
     );
 };
